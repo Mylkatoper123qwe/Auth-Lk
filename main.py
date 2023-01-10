7555a7d18b9aa1758fb3f992067c73fe6b4e7c25
Sadoza <saddmutro@gmail.com>
2023-01-10 20:04:03 +0200

    Change

---
+++ main.py
@@ -0,0 +1,40 @@
+#Значення  з текстом і  винятками.
+# while True:
+#     try:
+#         num = int(input("Ведіть число:"))
+#         print("Помилок немає")
+#     except:
+#         print("Помилка")
+
+#Середнє арефметичне
+# while True:
+#     try:
+#         b = []
+#         b = list(map(int, input("Ведіть 5 чисел: ").split()))
+#
+#         g = 0
+#         for key in b:
+#             g += key
+#         print(f"Підраховуємо: {g}")
+#
+#         print(g / len(b))
+#     except(ZeroDivisionError):
+#         print("Ви не вписали числа попробуйте ще.")
+
+# #Авторизація міні
+# while True:
+#     try:
+#         login = int(input("Ведіть ваш логін:"))
+#         auth = int(input("Ведіть ваш пароль:"))
+#         print("Без проблем,ви увійшли в систему")
+#     except(ValueError):
+#         print("Спробуйте щераз")
+
+
+
+
+
+
+
+
+
